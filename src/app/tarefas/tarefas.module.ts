db3ae548975db359e16cb93437fc1f7b078ba495
João Pedro <joao.ppedro430@gmail.com>
2020-08-01 00:08:30 -0300

    first commit

---
+++ src/app/tarefas/tarefas.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+
+import { TarefaService } from './shared';
+import { ListarTarefasComponent } from './listar';
+import { CadastrarTarefasComponent } from './cadastrar';
+import { EditarTarefasComponent } from './editar';
+
+@NgModule({
+  declarations: [ListarTarefasComponent, CadastrarTarefasComponent, EditarTarefasComponent],
+  imports: [
+    CommonModule,
+    RouterModule,
+    FormsModule
+  ],
+  providers: [
+    TarefaService
+  ]
+})
+export class TarefasModule { }
